1b2a48927fc17f50ac6d0642a0159be6f760a7bf
Brandon Harmon <harmonbc@muohio.edu>
2013-05-10 08:48:17 -0400

    Updating Sam's Changes

--- src/com/example/dcc/MainActivity.java
+++ src/com/example/dcc/MainActivity.java
@@ -1,59 +1,28 @@
 package com.example.dcc;
 
-import java.net.URISyntaxException;
-
-import com.example.dcc.helpers.User;
-import com.example.dcc.helpers.mysql.HttpConnection;
-import com.example.dcc.helpers.mysql.PreparedStatements;
-import com.example.dcc.helpers.mysql.PreparedStatements.SQL_COMMANDS;
-
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
-import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.LayoutInflater;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-	final private static int DIALOG_LOGIN = 1; 
-
 	//Create buttons Globally so they are available to all methods
-	private Button newsB;
-	private Button loginB;
-	private Button calB;
-	private Button mailB;
-	private Button photoB;
-	private Button reportB;
-	private Button actionB;
-	private Button directoryB;
-
-	private Context context;
-	private PreparedStatements sqlStatement; 
-	private LogInTask logTask;
-	
-	public String userText;
-	public String passwordText;
-
-
+	Button newsB;
+	Button loginB;
+	Button calB;
+	Button mailB;
+	Button photoB;
+	Button reportB;
+	Button actionB;
+	Button directoryB;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		context = this;
-		sqlStatement = new PreparedStatements(this.openOrCreateDatabase("dcc_settings", MODE_PRIVATE, null));
-		sqlStatement.runStatement(SQL_COMMANDS.createTable);
 
 		//These are the buttons on the left side of the screen.
 		//The have been initialized in order.
@@ -75,7 +44,6 @@ public class MainActivity extends Activity implements OnClickListener {
 		reportB.setOnClickListener(this);
 		actionB.setOnClickListener(this);
 		directoryB.setOnClickListener(this);
-
 	}
 
 	public void onClick(View v) {
@@ -86,7 +54,7 @@ public class MainActivity extends Activity implements OnClickListener {
 			startActivity(new Intent(this, AndroidRssReader.class));
 			break;
 		case R.id.button2:
-			showDialog(DIALOG_LOGIN, null);
+			startActivity(new Intent(this, MainActivity.class));
 			break;
 		case R.id.button3:
 			startActivity(new Intent(this, MainActivity.class));
@@ -101,142 +69,13 @@ public class MainActivity extends Activity implements OnClickListener {
 			startActivity(new Intent(this, LaunchActivity.class));
 			break;
 		case R.id.button7:
-			startActivity(new Intent(this, MainActivity.class));
+			 startActivity(new Intent(this, ActionItem.class));
+			 finish();
 			break;
 		case R.id.button8:
 			startActivity(new Intent(this, MainActivity.class));
 			finish();
 			break;
-		}
-	}
-
-	protected Dialog onCreateDialog(int id){
-		AlertDialog dialogDetails = null;
-
-		switch (id) {
-		case DIALOG_LOGIN:
-			LayoutInflater inflater = LayoutInflater.from(this);
-			View dialogview = inflater.inflate(R.layout.dialog_login, null);
-
-			AlertDialog.Builder dialogbuilder = new AlertDialog.Builder(this);
-			dialogbuilder.setTitle("Login");
-			dialogbuilder.setView(dialogview);
-			dialogDetails = dialogbuilder.create();
-
-			break;
-		}
-
-		return dialogDetails;
-	}
-
-	protected void onPrepareDialog(int id, Dialog dialog) {
-
-		switch (id) {
-		case DIALOG_LOGIN:
-			final AlertDialog alertDialog = (AlertDialog) dialog;
-			Button loginbutton = (Button) alertDialog
-					.findViewById(R.id.btn_login);
-			Button cancelbutton = (Button) alertDialog
-					.findViewById(R.id.btn_cancel);
-
-			final EditText user = (EditText) alertDialog.findViewById(R.id.login_text);
-			final EditText pwd = (EditText) alertDialog.findViewById(R.id.password);
-			
-			loginbutton.setOnClickListener(new View.OnClickListener() {
-
-				@Override
-				public void onClick(View v) {
-					Log.e(this.toString(), "Logging In");
-					
-					userText = user.getText().toString();
-					passwordText = pwd.getText().toString();
-					
-					logTask = null;
-					attemptLogin();
-					alertDialog.dismiss();
-				}
-			});
-			cancelbutton.setOnClickListener(new View.OnClickListener() {
-
-				@Override
-				public void onClick(View v) {
-					alertDialog.dismiss();
-				}
-			});
-			break;
-		}
-	}
-
-	/**
-	 * Attempts to sign in or register the account specified by the login form.
-	 * If there are form errors (invalid email, missing fields, etc.), the
-	 * errors are presented and no actual login attempt is made.
-	 */
-	public void attemptLogin() {
-		Log.e(this.toString(), "Attempt Log In");
-		
-		if (logTask != null) {
-			Log.e(this.toString(), "Return logTask != null");
-			return;
-		}
-
-		boolean cancel = false;
-		@SuppressWarnings("unused")
-		View focusView = null;
-
-		try{
-		// Check for a valid password.
-		if (TextUtils.isEmpty(passwordText)) {
-			Toast.makeText(context, "Password is Empty", Toast.LENGTH_LONG);
-			cancel = true;
-		} else if (passwordText.length() < 4) {
-			Toast.makeText(context, "Password is to short", Toast.LENGTH_LONG);
-			cancel = true;
-		}
-
-		// Check for a valid email address.
-		if (TextUtils.isEmpty(userText)) {
-			Toast.makeText(context, "User Name is Empty", Toast.LENGTH_LONG);;
-			cancel = true;
-		}
-
-		}catch(Exception e){
-			//Log.e(this.toString(), e.getMessage());
-			Log.i("stuff", userText+" "+passwordText);
-			cancel = true;
-		}
-		if (!cancel){
-			logTask = new LogInTask();
-			logTask.execute((Void) null);
-		}
-	}
-
-
-	public class LogInTask extends AsyncTask<Void, Void, Boolean>{
-
-		@Override
-		protected Boolean doInBackground(Void... params) {
-			Log.e("BG","in bg");
-			HttpConnection hc = new HttpConnection();
-			try {
-				User u = new User();
-				u.setCookieJar(hc.login(userText, passwordText));
-				hc.getFriends(u.getCookieJar());
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return null;
-		}
-
-		@Override
-		protected void onPostExecute(final Boolean success) {
-
-		}
-
-		@Override
-		protected void onCancelled() {
-
 		}
 	}
 
--- src/com/example/dcc/RSSFeed.java
+++ src/com/example/dcc/RSSFeed.java
@@ -4,28 +4,28 @@ import java.util.List;
 import java.util.Vector;
 
 public class RSSFeed {
-	private String title = null;
-	private String description = null;
-	private String link = null;
-	private String pubdate = null;
-	private List<RSSItem> itemList;
-	
+	private static String title = null;
+	private static  String description = null;
+	private static String link = null;
+	private static  String pubdate = null;
+	private static  List<RSSItem> itemList;
+
 	RSSFeed(){
 		itemList = new Vector<RSSItem>(0);
 	}
-	
+
 	void addItem(RSSItem item){
 		itemList.add(item);
 	}
-	
+
 	RSSItem getItem(int location){
 		return itemList.get(location);
 	}
-	
+
 	List<RSSItem> getList(){
 		return itemList;
 	}
-	
+
 	void setTitle(String value)
 	{
 		title = value;
@@ -42,7 +42,7 @@ public class RSSFeed {
 	{
 		pubdate = value;
 	}
-	
+
 	String getTitle()
 	{
 		return title;
