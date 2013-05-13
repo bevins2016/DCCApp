2c9b076552d9738be6c04942ecc3210140e5ecac
Brandon Harmon <harmonbc@muohio.edu>
2013-05-13 14:41:17 -0400

    Revert "Fixing"
    This reverts commit 879493e09c7325ecb18a0ee0412aaca5307e02cf.

---
+++ src/com/example/dcc/helpers/Links.java
@@ -0,0 +1,5 @@
+package com.example.dcc.helpers;
+
+public enum Links {
+	PROFILE, MESSAGES, MEDIA, FRIENDS, GROUPS, MEMBERS, SETTINGS
+}
--- src/com/example/dcc/helpers/User.java
+++ src/com/example/dcc/helpers/User.java
@@ -2,11 +2,15 @@ package com.example.dcc.helpers;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.http.client.CookieStore;
 import org.apache.http.cookie.Cookie;
 
+import com.example.dcc.helpers.hacks.DCCCookieStore;
+
+import android.graphics.Bitmap;
 import android.util.Log;
 
 public class User implements Serializable{
@@ -15,16 +19,25 @@ public class User implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = -5285360029686080283L;
+	private static final String LOG = "User Object";
 	private String handle;
 	private String name;
-	public DCCCookieStore cookieJar;
 	public String cookies;
-	
+	private Bitmap image;
+	public List<DccCookie> cook;
+
 	public User(){
-		this.name = null;
-		this.cookieJar = new DCCCookieStore();
-		this.handle = null;
-		this.cookies = null;
+		this.name = "";
+		this.handle = "";
+		this.cookies = "";
+		this.image = null;
+		this.cook = new LinkedList<DccCookie>();
+	}
+	
+	public void displayData(){
+		Log.i(LOG, name);
+		Log.i(LOG, handle);
+		Log.i(LOG, cookies);
 	}
 
 	public String getHandle() {
@@ -35,25 +48,41 @@ public class User implements Serializable{
 		this.handle = handle;
 	}
 
-
-	public DCCCookieStore getCookieJar() {
-		Log.e("Cookie", cookieJar.getCookies().size()+"");
-		return cookieJar;
-	}
-
-	public void setCookieJar(DCCCookieStore cj) {
-		Log.e("Cookie", "Setting it "+cj.getCookies().size());
-		List<Cookie> al = cj.getCookies();
-		for(Cookie c: al){
-			cookieJar.addCookie(c);
-		}
-	}
-
 	public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getURL(Links link){
+		String base = "/members/"+handle+"/";
+		switch(link){
+		case PROFILE:
+			return "/intern/";
+		case MESSAGES:
+			return base+"messages/";
+		case MEDIA:
+			return base+"media/";
+		case FRIENDS:
+			return base+"friends/";
+		case GROUPS:
+			return base+"groups/";
+		case MEMBERS:
+			return "/members/";
+		case SETTINGS:
+			return "/settings/";
+		default:
+			return "/intern/";
+		}
+	}
+
+	public Bitmap getImage() {
+		return image;
+	}
+
+	public void setImage(Bitmap image) {
+		this.image = image;
 	}
 }
