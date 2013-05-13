4e50ec6dbeb298c0f904ed3a20e685341e8a1e1c
Brandon Harmon <harmonbc@muohio.edu>
2013-05-13 10:29:14 -0400

    Removed Dialog

--- src/com/example/dcc/MainActivity.java
+++ src/com/example/dcc/MainActivity.java
@@ -24,11 +24,8 @@ import com.example.dcc.helpers.mysql.HttpConnection;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-	final private static int DIALOG_LOGIN = 1; 
-
 	//Create buttons Globally so they are available to all methods
 	private Button newsB;
-	private Button loginB;
 	private Button calB;
 	private Button photoB;
 	private Button reportB;
@@ -37,7 +34,6 @@ public class MainActivity extends Activity implements OnClickListener {
 	private Button searchB;
 
 	private Context context;
-	private LogInTask logTask;
 
 	public String userText;
 	public String passwordText;
@@ -103,62 +99,6 @@ public class MainActivity extends Activity implements OnClickListener {
 			finish();
 			break;
 		}
-	}
-
-	/**
-	 * This method is used to display the dialog that accepts the user's
-	 * name and password and starts the authentication task.
-	 */
-	protected Dialog onCreateDialog(int id){
-		AlertDialog dialogDetails = null;
-
-		switch (id) {
-		case DIALOG_LOGIN:
-			LayoutInflater inflater = LayoutInflater.from(this);
-			View dialogview = inflater.inflate(R.layout.dialog_login, null);
-			AlertDialog.Builder dialogbuilder = new AlertDialog.Builder(this);
-			dialogbuilder.setTitle("DCC Login").setView(dialogview);
-			dialogDetails = dialogbuilder.create();
-		}
-		return dialogDetails;
-	}
-
-	/**
-	 * This class is used to build the dialog box prior to being set to visible.
-	 * All click listeners in the dialog is limited to this method(does not use this as
-	 * the listener). Ignores the int 'id'
-	 */
-	protected void onPrepareDialog(int id, Dialog dialog) {
-
-		//Create dialog
-		final AlertDialog alertDialog = (AlertDialog) dialog;
-		Button loginbutton = (Button) alertDialog.findViewById(R.id.btn_login);
-		Button cancelbutton = (Button) alertDialog.findViewById(R.id.btn_cancel);
-
-		//Get input fields
-		final EditText user = (EditText) alertDialog.findViewById(R.id.login_text);
-		final EditText pwd = (EditText) alertDialog.findViewById(R.id.password);
-
-		//Set the listener for the login action
-		loginbutton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				userText = user.getText().toString();
-				passwordText = pwd.getText().toString();
-
-				logTask = null;
-				attemptLogin();
-				alertDialog.dismiss();
-			}
-		});
-		
-		//Set the lisener for the cancel action
-		cancelbutton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				alertDialog.dismiss();
-			}
-		});
 	}
 
 	/*
