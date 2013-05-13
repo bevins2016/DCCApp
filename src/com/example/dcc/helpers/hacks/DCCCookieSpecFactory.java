e45294cdad19af8def5bc0d1cf7dbdd23d7e1378
Sam Bevins <bevins2012@hotmail.com>
2013-05-13 11:25:13 -0400

    none

---
+++ src/com/example/dcc/helpers/hacks/DCCCookieSpecFactory.java
@@ -0,0 +1,15 @@
+package com.example.dcc.helpers.hacks;
+
+import org.apache.http.cookie.CookieSpec;
+import org.apache.http.cookie.CookieSpecFactory;
+import org.apache.http.params.HttpParams;
+
+public class DCCCookieSpecFactory implements CookieSpecFactory{
+
+	@Override
+	public CookieSpec newInstance(HttpParams params) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
