946740d23c5639e43fbf432a757884d4afd654e0
harmonbc <harmonbc@miamioh.edu>
2013-05-30 11:19:39 -0400

    created custom array adapters

---
+++ src/com/example/dcc/helpers/BitmapCache.java
@@ -0,0 +1,7 @@
+package com.example.dcc.helpers;
+
+/**
+ * Created by harmonbc on 5/30/13.
+ */
+public class BitmapCache {
+}
---
+++ src/com/example/dcc/helpers/hacks/NewsArrayAdapter.java
@@ -0,0 +1,7 @@
+package com.example.dcc.helpers.hacks;
+
+/**
+ * Created by harmonbc on 5/30/13.
+ */
+public class NewsArrayAdapter {
+}
