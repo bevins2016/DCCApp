ec275cefa9cfc7b473b1baa32a847078faeafa71
harmonbc <harmonbc@miamioh.edu>
2013-05-30 15:36:13 -0400

    added detailed edailies

---
+++ src/com/example/dcc/EDailyDetailedActivity.java
@@ -0,0 +1,13 @@
+package com.example.dcc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by harmonbc on 5/30/13.
+ */
+public class EDailyDetailedActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ src/com/example/dcc/fragment/EDailyDetailFragment.java
@@ -0,0 +1,7 @@
+package com.example.dcc.fragment;
+
+/**
+ * Created by harmonbc on 5/30/13.
+ */
+public class EDailyDetailFragment {
+}
